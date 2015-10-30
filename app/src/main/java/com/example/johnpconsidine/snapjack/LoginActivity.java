c3b4674d6dbed70fbb871c72f49a6246119cb3a1
John P Considine <johnpconsidine@johns-mbp.dhcp.nd.edu>
2015-10-30 19:41:08 -0400

    android stuff

---
+++ app/src/main/java/com/example/johnpconsidine/snapjack/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.johnpconsidine.snapjack;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.johnpconsidine.snapjack.R;
+import com.example.johnpconsidine.snapjack.SignupActivity;
+
+public class LoginActivity extends Activity {
+
+    protected TextView mSignUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSignUpTextView = (TextView)findViewById(R.id.signupText);
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/johnpconsidine/snapjack/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.johnpconsidine.snapjack;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+    }
+
+
+}
